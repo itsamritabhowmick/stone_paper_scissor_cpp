7914ee2731b97b74b18b95a155e2ef962c1c9456
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2024-10-14 21:47:21 +0530

    created variable

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+    int score=5;
+    int computer_score=0;
+    int user_score=0;
+    int user_input;
+    int computer_input;
+
+}
